8989ab128529251996b23ade23902e082d17e454
Ekaanth <akanth1994@gmail.com>
2023-11-14 23:45:13 +0530

    added prettier + lint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_|^req|^next' }],
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/prop-types': 0,
+    'no-empty-function': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended', // If using TypeScript.
+        'plugin:prettier/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json', // Path to your TypeScript config file.
+      },
+    },
+  ],
+};
